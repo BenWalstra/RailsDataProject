a2a06c606e91584d3ae2d3341680cff111b371a0
BenWalstra <benwalstra@gmail.com>
2019-10-10 09:53:36 -0500

    added inbetween table for pokemon and types

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -3,7 +3,8 @@
 class Pokemon < ApplicationRecord
   validates :name, :hp, :attack, :defence, :speed, :spAttack, :spDefense, presence: true
   validates :name, uniqueness: true
-  belongs_to :type
+
+  has_many :type, through: :pokemon_types
   has_many :move, through: :pokemon_move
   has_many :trainer, through: :trainer_pokemon
 end
