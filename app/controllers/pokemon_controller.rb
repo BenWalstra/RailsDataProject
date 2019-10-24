c183da84c91e64f6e3c05e45d2d14ca7cb6e3efd
BenWalstra <benwalstra@gmail.com>
2019-10-24 17:45:57 -0500

    addded pagnation

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,9 +1,13 @@
+# frozen_string_literal: true
+
 class PokemonController < ApplicationController
+  include Pagy::Backend
   def index
+    all = Pokemon.order(:name)
+    @pagy, @pokemon = pagy(all, items: 20)
   end
 
-  def show
-  end
+  def show; end
 
   def search_results
     @query = params[:query]
